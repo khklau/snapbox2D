98521b2e7138820526b308544046cd495a5da165
Kean H Lau <khklau@users.noreply.github.com>
2017-06-02 18:28:48 +1000

    adding initial implementation of snapshot

---
+++ Box2D/SnapBox2D/snapshot.cxx
@@ -0,0 +1,15 @@
+#include <SnapBox2D/snapshot.hpp>
+#include <Box2D/Dynamics/b2World.h>
+#include <turbo/cinterop/untyped_allocator.hpp>
+
+namespace tci = turbo::cinterop;
+
+namespace snapbox2d {
+
+snapshot::snapshot(const tci::untyped_allocator& original, const b2World& world)
+    :
+	heap(new tci::untyped_allocator(original)),
+	step(world.GetProfile().step)
+{ }
+
+} // namespace snapbox2d
---
+++ Box2D/SnapBox2D/snapshot.hpp
@@ -0,0 +1,29 @@
+#ifndef SNAPBOX2D_SNAPSHOT_HPP
+#define SNAPBOX2D_SNAPSHOT_HPP
+
+#include <cstdint>
+#include <memory>
+
+class b2World;
+
+namespace turbo {
+namespace cinterop {
+
+class untyped_allocator;
+
+} // namespace cinterop
+} // nemespace turbo
+
+namespace snapbox2d {
+
+struct snapshot
+{
+    snapshot() = delete;
+    snapshot(const turbo::cinterop::untyped_allocator& original, const b2World& world);
+    std::unique_ptr<turbo::cinterop::untyped_allocator> heap;
+    float step;
+};
+
+} // namespace snapbox2d
+
+#endif
